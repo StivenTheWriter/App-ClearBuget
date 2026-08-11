da7139399481ae89c997e3a1952f3112078942c7
Your Name <you@example.com>
2026-08-10 23:07:12 -0400

    chore(gitignore): excluir archivos temporales

--- ProyectMoney.FrontView/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using Fonts;
-global using ProyectMoney.FrontView.Data;
-global using ProyectMoney.FrontView.PageModels;
-global using ProyectMoney.FrontView.Pages;
-global using ProyectMoney.FrontView.Services;
-global using ProyectMoney.FrontView.Utilities;
